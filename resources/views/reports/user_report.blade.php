1a466c798b44e1ff93a8a8a69b13428e415e6bbf
justslick23 <hello@tokelofoso.online>
2025-05-23 23:35:30 +0200

    commit

--- resources/views/reports/user_report.blade.php
+++ resources/views/reports/user_report.blade.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Monthly Report - {{ $user->name }}</title>
     <style>
         * {
@@ -279,6 +282,8 @@
             }
         }
     </style>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet">
+
 </head>
 <body>
     <div class="report-container">
